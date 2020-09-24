11a53c2e18632e63911754c625169b5b609c7397
mikejorourke <michael.orourke@g.austincc.edu>
2020-09-24 21:52:29 +0000

    HW 2 and unit testing.

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,63 @@
-//cpp
+#include "decision.h"
+#include <iostream>
+
+using std::cout; using std::cin;
+
+std::string get_letter_grade_using_if(int grade)
+{
+    std::string  if_grade;
+
+    if(grade > 90 && grade < 100)
+    {
+        if_grade = "A";
+    }
+    else if(grade > 80 && grade < 89)
+    {
+        if_grade = "B";
+    }
+    else if(grade > 70 && grade < 79)
+    {
+        if_grade = "C";
+    }
+    else if(grade > 60 && grade < 69)
+    {
+        if_grade = "D";
+    }
+    else if(grade > 00 && grade < 59)
+    {
+        if_grade = "F";
+    }
+
+    return if_grade;
+}
+
+std::string get_letter_grade_using_switch(int grade)
+{
+    std::string switch_grade;
+    
+    switch (grade/10) 
+    {
+    case 9:
+        switch_grade = "A";
+        break;
+    case 8:
+        switch_grade = "B";
+        break;
+    case 7:
+        switch_grade = "C";
+        break;
+    case 6:
+        switch_grade = "D";
+        break;
+    case 5:
+    case 4:
+    case 3:
+    case 2:
+    case 1:
+    case 0:
+        switch_grade = "F";
+        break;
+    }
+
+  return switch_grade;  
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,20 @@
 //Write the include statement for decisions.h here
-
+#include "decision.h"
+#include <iostream>
 //Write namespace using statements for cout and cin
+using std::cout; using std::cin;
 
 int main() 
 {
+   int user_grade;
+   
+    cout<<"Enter Your Test Score: ";
+    cin>>user_grade; 
 
-	return 0;
+    cout<< "Your letter grade is:" <<get_letter_grade_using_if(user_grade) << " " << get_letter_grade_using_switch(user_grade);
+
+    return 0;
+  
+ 
+
 }
-
