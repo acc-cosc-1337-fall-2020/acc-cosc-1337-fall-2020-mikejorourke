7ffcdc022b0d946d765ac6103e1249d755c2f6ef
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-10-26 17:13:56 -0500

    tictactoe1 grading

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,14 +1,27 @@
-#include "tic_tac_toe_3.h"
+#include "tic_tac_toe.h"
+
 
 int main() 
 {
-	tic_tac_toe_board boardObj;
-	string first = "";
-	cout<< "Enter X or O";
-	cin>> first;
-	
-	boardObj.start_game(first);	
-	
+	TicTacToe tic_tac_toe;
+	std::string player;
+
+	std::cout<<"Enter X or O: ";
+	std::cin>>player;
+
+	tic_tac_toe.start_game(player);
+
+	do
+	{
+		int position;
+		std::cout<<"Enter position from 1 to 9: ";
+		std::cin>>position;
+		tic_tac_toe.mark_board(position);
+		tic_tac_toe.display_board();
+
+	} while (tic_tac_toe.game_over() == false);
+
+	std::cout<<"Game over";	
 
 	return 0;
 }
--- test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
+++ test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "tic_tac_toe_3.h"
+#include "tic_tac_toe.h"
 
 TEST_CASE("Verify Test Configuration", "verification") 
 {
@@ -9,15 +9,23 @@ TEST_CASE("Verify Test Configuration", "verification")
 
 TEST_CASE("tic_tac_toe test function") 
 {
-	tic_tac_toe_board test;
+	TicTacToe test;
 	test.mark_board(1);
+	REQUIRE(test.game_over() == false);
 	test.mark_board(2);
+	REQUIRE(test.game_over() == false);
 	test.mark_board(3);
+	REQUIRE(test.game_over() == false);
 	test.mark_board(4);
+	REQUIRE(test.game_over() == false);
 	test.mark_board(5);
+	REQUIRE(test.game_over() == false);
 	test.mark_board(6);
+	REQUIRE(test.game_over() == false);
 	test.mark_board(7);
+	REQUIRE(test.game_over() == false);
 	test.mark_board(8);
+	REQUIRE(test.game_over() == false);
 	test.mark_board(9);
-	REQUIRE(test.tic_tac_toe_board :: check_board_full() == true);
+	REQUIRE(test.game_over() == true);
 }
